24d86ff5342d8427e41823bac77edd75d8a9a72d
huynhtantai782 <huynhtantai.uit@gmail.com>
2016-06-02 18:27:05 +0700

    Update home.controller.js

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -6,7 +6,29 @@ angular.module('demoApp')
     var vm = this;
 
     angular.extend(vm, {
-      name: 'HomeCtrl'
+      name: 'HomeCtrl',
+      onClick: function () {
+  		var msgBox = $uibModal.open( {
+			templateUrl: 'components/msgbox/msgbox.html',
+			controller: 'MsgBoxController',
+			animation: true,
+            size: 'sm',
+            // send data to MsgBoxController
+            resolve: {
+              	items: function() {
+                    return {
+                    	title: "Loi",
+                    	msgType: "INFO",
+                    	message: "LOI HE THONG",
+                    	btnType: "YESNO"
+                    };
+              	}
+            }
+		});
+		msgBox.result.then(function () {
+			debugger;
+		});
+      },
     });
 
   });
